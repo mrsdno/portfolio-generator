2f433ca07f4fa5871bceda86593ea72dc841a6f2
mrsdno <christine@dno.dev>
2022-04-26 22:08:37 -0400

    generate webpage using fs

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-    for (let i=0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const [name, github] = profileDataArgs;
 
-    console.log('================')
+const generatePage = require('./src/page-template.js')
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
+
+
+// console.log(profileDataArgs);
+
+// const printProfileData = (profileDataArr) => {
+//     for (let i=0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('================')
+
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
